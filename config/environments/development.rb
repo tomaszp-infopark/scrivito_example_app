c0daca5725e624f33d3486d1cfd2d573a98f5cc7
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2016-10-27 11:30:59 +0200

    Assets without digets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.assets.debug = true
-  config.assets.digest = true
+  config.assets.digest = false
   config.assets.raise_runtime_errors = true
   config.cache_classes = false
   config.consider_all_requests_local = true
